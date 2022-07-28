2090556a6ce34f18550e8f73c6f0a8363a2e45b6
AbelMartinezFlores <abelmartinezlamola@gmail.com>
2022-07-28 14:46:53 +0200

    estilo y logica para varios carousel a la vez y responsive acabado

--- scripts/capitulos.js
+++ scripts/capitulos.js
@@ -1,48 +1,70 @@
-const carousel = document.querySelector('.container-carousel');
-const episodes = document.querySelectorAll('.episode');
+// ------------- todo lo necesario para el carousel -------------
 
-const leftArrow = document.querySelector('#left-arrow');
-const rightArrow = document.querySelector('#right-arrow');
+// ------------- aqui empiezo con las flechas de todos los carousel -------------
+const leftArrow = document.querySelectorAll('.left-arrow');
+const rightArrow = document.querySelectorAll('.right-arrow');
 
-rightArrow.addEventListener('click', () => {
-    carousel.scrollLeft += carousel.offsetWidth;
+rightArrow.forEach( (e) => {
 
-    const activeInidicator = document.querySelector('.indicators .active');
-    let nextIndicator = activeInidicator.nextSibling;
-    if(nextIndicator){
-        nextIndicator.classList.add('active');
-        activeInidicator.classList.remove('active');
-    }
-});
+    e.addEventListener('click', (e) => {
+        //para determinar el carousel pulsado y sacar que temporada es
+        let carousel = e.currentTarget.previousSibling.previousSibling;
+        let season = carousel.dataset.season;
 
-leftArrow.addEventListener('click', () => {
-    carousel.scrollLeft -= carousel.offsetWidth;
+        //movemos scroll a la derecha
+        carousel.scrollLeft += carousel.offsetWidth;
 
-    const activeInidicator = document.querySelector('.indicators .active');
-    let prevIndicator = activeInidicator.previousSibling;
-    if(prevIndicator){
-        prevIndicator.classList.add('active');
-        activeInidicator.classList.remove('active');
-    }
-});
-
-//paginacion del carousel
-const numberPages = Math.ceil(episodes.length/5);
-for( let i = 0 ; i < numberPages; i++){
-
-    const indicator = document.createElement('div');
-
-    if (i === 0){
-        indicator.classList.add('active');
-    }
-
-    document.querySelector('.indicators').appendChild(indicator);
-
-    indicator.addEventListener('click', (e) => {
-        carousel.scrollLeft = i* carousel.offsetWidth;
-
-        document.querySelector('.indicators .active').classList.remove('active');
-        e.target.classList.add('active');
-
+        //encontramos indicador activado y lo desactivamos y activamos el siguiente
+        const activeInidicator = document.querySelector(`#${season} .indicators .active`);
+        let nextIndicator = activeInidicator.nextSibling;
+        if(nextIndicator){
+            nextIndicator.classList.add('active');
+            activeInidicator.classList.remove('active');
+        }
     });
-}
+});
+
+leftArrow.forEach( (e) =>{
+    e.addEventListener('click', (e) => {
+        //para determinar el carousel pulsado y sacar que temporada es
+        let carousel = e.currentTarget.nextSibling.nextSibling;
+        let season = carousel.dataset.season;
+
+        //movemos scroll a la derecha
+        carousel.scrollLeft -= carousel.offsetWidth;
+
+        //encontramos indicador activado y lo desactivamos y activamos el anterior
+        const activeInidicator = document.querySelector(`#${season} .indicators .active`);
+        let prevIndicator = activeInidicator.previousSibling;
+        if(prevIndicator){
+            prevIndicator.classList.add('active');
+            activeInidicator.classList.remove('active');
+        }
+    });
+});
+
+
+// ------------- Paginacion de los episodios de cada carousel -------------
+const containerCarousels = document.querySelectorAll('.carousel');
+containerCarousels.forEach( (e) => {
+    //sacamos que carousel es y vemoc suantos episodios tiene esa temporada para dividir la paginacion
+    let season = e.attributes.id.nodeValue
+    const numberPages = Math.ceil(e.childElementCount/5);
+
+    for( let i = 0 ; i < numberPages; i++){
+
+        const indicator = document.createElement('div');
+        //para que inicialmente el primer indicador sea el marcado
+        if (i === 0) indicator.classList.add('active');
+    
+        document.querySelector(`#${season} .indicators`).appendChild(indicator);
+        //para que todos los indicadores permitan desplazamiento
+        indicator.addEventListener('click', (e) => {
+            carousel.scrollLeft = i* carousel.offsetWidth;
+            document.querySelector(`#${season} .indicators .active`).classList.remove('active');
+            e.target.classList.add('active');
+        });
+    }   
+});
+
+// ---------- Programacion dinamica web ----------
